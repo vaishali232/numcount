eb8827a56c94286397763617af36b79f84922d29
vaishali232 <51689354+vaishali232@users.noreply.github.com>
2019-08-08 13:23:55 +0530

    Create num.c

---
+++ num.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char s[1000];
+int n,count=0;
+scanf("%[^\n]%*c",s);
+n=strlen(s);
+for(int i=0;i<n;i++)
+{
+    if(s[i]>=0&&s[i]<=9)
+    {
+        count++;
+    }
+}
+printf("%d",count);
+
+
+}
